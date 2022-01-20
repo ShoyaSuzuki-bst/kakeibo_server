76596f6d2e7bd6482a0c136fcbecd347c94dbd00
ShoyaSuzuki-bst <sh.suzuki@buysell-technologies.com>
2022-01-21 00:35:06 +0900

    ユーザーのCRUD追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,34 @@
 class UsersController < ApplicationController
   skip_before_action :authenticate, only: :create
+  before_action :get_user
 
-  def show; end
+  def show
+    render json: @user
+  end
 
-  def create; end
+  def create
+    decoded_token = FirebaseUtils::Auth.verify_id_token(token)
+    @user = User.create!(user_params.merge(firebase_uid: decoded_token['uid']))
+    render json: @user
+  end
 
-  def update; end
+  def update
+    @user.update!(user_params)
+    render json: @user
+  end
 
-  def destroy; end
+  def destroy
+    @user.destroy!
+    render json: {status: 'ok'}
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email)
+  end
+
+  def get_user
+    @user = User.find(params[:id])
+  end
 end
